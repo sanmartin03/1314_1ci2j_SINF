c2891f8d19cc6bb71396d143138f039998e6e840
sanmartin03 <alu53225876n@ieselcaminas.org>
2014-04-10 13:31:37 +0200

    new sanity check. checks if mencoder is installed

--- bulkonvert.sh
+++ bulkonvert.sh
@@ -6,48 +6,53 @@
 
 # Sanity checks
 # ...
-
-if [ $1 == "-h" ]; 
+if [ ! -x /usr/bin/mencoder  ];
 then
-  echo ""
-  echo "you must writte ./bulkonvert.sh [path]"
-  echo "the path is the way to find the folder containing the video files"
-  echo ""
-  exit 1
+  echo "You must being instaled mencoder. Install it and try again."
+  echo "run  'sudo apt-get install mencoder' for install it"
 else
-  if [ $# != 1 ] ; 
+  if [ $1 == "-h" ]; 
   then
     echo ""
-    echo "bad parameter count, you must write the origin folder"
-    echo "you could write ./bulkonvert.sh -h for help"
+    echo "you must writte ./bulkonvert.sh [path]"
+    echo "the path is the way to find the folder containing the video files"
     echo ""
     exit 2
-    else
-      cadena1=$1
-      long=${#cadena1} 
-      if [ ${cadena1:long-1:1} != "/" ];
-      then
-	echo "you don't write a valid path. example: home/videos/"   
+  else
+    if [ $# != 1 ] ; 
+    then
+      echo ""
+      echo "bad parameter count, you must write the origin folder only"
+      echo "you could write ./bulkonvert.sh -h for help"
+      echo ""
       exit 3
       else
-        if ! [ -d $1 ];
+        cadena1=$1
+        long=${#cadena1} 
+        if [ ${cadena1:long-1:1} != "/" ];
         then
-	  echo "the folder you write not exist"   
+	  echo "you don't write a valid path. example: home/videos/"   
         exit 4
         else
+          if ! [ -d $1 ];
+          then
+	    echo "the folder you write not exist"   
+          exit 5
+          else
 
 # Main code
 
-        ls -1 $1*.avi > videofiles
+          ls -1 $1*.avi > videofiles
 
-         while read videofile
-         do
+           while read videofile
+           do
 	    lengthname=${#videofile}
 	    namefile=${videofile:0:$lengthname-4}
   	    mencoder "$videofile" -o "$namefile".mp4 -oac mp3lame -ovc lavc -lavcopts vcodec=mpeg1video -of mpeg
 
-         done < videofiles
-         rm videofiles
+          done < videofiles
+          rm videofiles
+        fi
       fi
     fi
   fi
